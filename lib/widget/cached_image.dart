4c6e6a1298c385506436ac6ac02d2237fa162e54
baikourin <bai@curama.jp>
2020-01-23 17:08:55 +0900

    cached_iamge widgetの作成

---
+++ lib/widget/cached_image.dart
@@ -0,0 +1,45 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class CachedImage extends StatelessWidget {
+  final AlignmentGeometry alignment;
+  final BoxFit fit;
+  final String imageUrl;
+  final double width;
+  final double height;
+  final bool inSizeBox;
+
+  const CachedImage({
+    Key key,
+    @required this.imageUrl,
+    this.alignment,
+    this.fit,
+    this.width,
+    this.height,
+    this.inSizeBox = false,
+  })  : assert(imageUrl != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return inSizeBox
+        ? FractionallySizedBox(
+            widthFactor: 1,
+            child: CachedNetworkImage(
+              alignment: alignment,
+              fit: fit,
+              imageUrl: imageUrl,
+              width: width,
+              height: height,
+            ),
+          )
+        : CachedNetworkImage(
+            alignment: alignment,
+            fit: fit,
+            imageUrl: imageUrl,
+            width: width,
+            height: height,
+          );
+  }
+}
