0c4e0676ee19682d927ba59d3ca1486c824213f8
baikourin <baikourim@yahoo.co.jp>
2020-01-29 00:00:09 +0900

    sales_boxの追加

---
+++ lib/widget/sales_box.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:tour/model/common_model.dart';
+import 'package:tour/model/sales_box_model.dart';
+import 'package:tour/util/navigator_util.dart';
+import 'package:tour/widget/cached_image.dart';
+import 'package:tour/widget/webview.dart';
