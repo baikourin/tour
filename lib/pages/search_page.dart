a14006cedb4aa9e87dd07771cf2faefd96c1a61f
baikourin <baikourim@yahoo.co.jp>
2020-01-29 23:30:52 +0900

    search_pageの微修正

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -2,8 +2,36 @@ import 'package:flutter/material.dart';
 
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:tour/model/common_model.dart';
 
+const TYPES = [
+  'channelgroup',
+  'gs',
+  'plane',
+  'train',
+  'cruise',
+  'district',
+  'food',
+  'hotel',
+  'huodong',
+  'shop',
+  'sight',
+  'ticket',
+  'travelgroup'
+];
+const SEARCH_BAR_DEFAULT_TEXT = '网红打卡地 景点 酒店 美食';
+
+///搜索页面
 class SearchPage extends StatefulWidget {
+  final bool hideLeft;
+  final String searchUrl;
+  final String keyword;
+  final String hint;
+
+  SearchPage(
+    {Key key, this.hideLeft, this.searchUrl, this.keyword, this.hint})
+    : super(key: key);
+
   @override
   _SearchPageState createState() => _SearchPageState();
 }
@@ -46,24 +74,4 @@ class _SearchPageState extends State<SearchPage> {
   }
 }
 
-class CommonModel {
-  final String icon;
-  final String title;
-  final String url;
-  final String statusBarColor;
-  final bool hideAppBar;
-
-  CommonModel(
-      {this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar});
-
-  factory CommonModel.fromJson(Map<String, dynamic> json) {
-    return CommonModel(
-      icon: json['icon'],
-      title: json['title'],
-      url: json['url'],
-      statusBarColor: json['statusBarColor'],
-      hideAppBar: json['hideAppBar'],
-    );
-  }
-}
 
