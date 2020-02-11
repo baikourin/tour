0d4b5d0eec255f8bc05b4c040a6142be8d39ab09
baikourin <baikourim@yahoo.co.jp>
2020-02-12 00:28:44 +0900

    Android调用Flutter依赖成功。

---
+++ android/asr_plugin/src/main/java/com/kourim/plugin/asr/AsrPlugin.java
@@ -0,0 +1,12 @@
+package com.kourim.plugin.asr;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+public class AsrPlugin implements MethodChannel.MethodCallHandler {
+    // Flutter 依赖成功
+    @Override
+    public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+
+    }
+}
--- lib/plugin/asr_manager.dart
+++ lib/plugin/asr_manager.dart
@@ -4,17 +4,17 @@ import 'package:flutter/services.dart';
 class AsrManager {
   static const MethodChannel _channel = const MethodChannel('asr_plugin');
 
-  ///开始录音
+  ///録音開始
   static Future<String> start({Map params}) async {
     return await _channel.invokeMethod('start', params ?? {});
   }
 
-  ///停止录音
+  ///録音停止
   static Future<String> stop() async {
     return await _channel.invokeMethod('stop');
   }
 
-  ///取消录音
+  ///録音キャンセル
   static Future<String> cancel() async {
     return await _channel.invokeMethod('cancel');
   }
