45aa4451a9e5cbdac3d011d57761467380fff5d4
baikourin <baikourim@yahoo.co.jp>
2020-02-15 17:19:59 +0900

    plugin Android端的实现。

--- android/app/src/main/java/com/kourim/tour/MainActivity.java
+++ android/app/src/main/java/com/kourim/tour/MainActivity.java
@@ -1,6 +1,9 @@
 package com.kourim.tour;
 
 import android.os.Bundle;
+
+import com.kourim.plugin.asr.AsrPlugin;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -9,5 +12,11 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    // 注册自己的plugin
+    registerSelfPlugin();
+  }
+
+  private void registerSelfPlugin() {
+    AsrPlugin.registerWith(registrarFor("com.kourim.plugin.asr.AsrPlugin"));
   }
 }
--- android/asr_plugin/src/main/java/com/kourim/plugin/asr/ResultStateful.java
+++ android/asr_plugin/src/main/java/com/kourim/plugin/asr/ResultStateful.java
@@ -1,32 +1,58 @@
 package com.kourim.plugin.asr;
 
+import androidx.annotation.Nullable;
+import android.util.Log;
+
 import io.flutter.plugin.common.MethodChannel;
 
 public class ResultStateful implements MethodChannel.Result {
     private final static String TAG = "ResultStateful";
     private MethodChannel.Result result;
-
-    private ResultStateful(MethodChannel.Result result) {
-        this.result = result;
-    }
+    private boolean called;
 
     // 外界只能通过这个of方法来创建
     public static ResultStateful of(MethodChannel.Result result){
         return new ResultStateful(result);
     }
 
-    @Override
-    public void success(Object result) {
-
+    private ResultStateful(MethodChannel.Result result) {
+        this.result = result;
     }
 
     @Override
-    public void error(String errorCode, String errorMessage, Object errorDetails) {
+    public void success(@Nullable  Object o) {
+        if (called){
+            printError();
+            return;
+        }
+        called=true;
+        result.success(o);
+    }
+
+    @Override
+    public void error(String s, @Nullable String s1, @Nullable Object o) {
+        if (called){
+            printError();
+            return;
+        }
+        called=true;
+        result.error(s, s1, o);
 
     }
 
+    // 没有实现
     @Override
     public void notImplemented() {
+        if (called){
+            printError();
+            return;
+        }
+        called=true;
+        result.notImplemented();
 
+    }
+
+    private void printError() {
+        Log.e(TAG, "error: result called");
     }
 }
